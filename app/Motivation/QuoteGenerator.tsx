2a98d89dfd9ea3d8015ff4f94d4c3ccee385caf9
ali <4lisaeed96@gmail.com>
2024-02-10 03:14:10 -0600

    still working on motivation section

--- app/Motivation/QuoteGenerator.tsx
+++ app/Motivation/QuoteGenerator.tsx
@@ -1,34 +1,43 @@
 "use client"
 import { use, useEffect, useState } from "react";
 import './motivation.css';
+import OpenAI from 'openai';
 
-const openAi = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY;
 
 export function QuoteGenerator() {
-    console.log(openAi);
+    const apiKey = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY;
+    const openAi = new OpenAI({apiKey, timeout: 10000, dangerouslyAllowBrowser: true});
     const [quote, setQuote] = useState("");
-    useEffect(() => {
         const generateQuote = async () => {
-            const res = await fetch("https://api.openai.com/v1/chat/completions",{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${openAi}`
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                message: [{role: "system", content: "give me a quote that will help a person get positively motivated to go to the gym. keep it one sentence long."}]
-            })   
-            })
-            const data = await res.json();
-            setQuote(data.content);
-            console.log(data.content);
+            try {
+                const res = await openAi.chat.completions.create({
+                    model: "gpt-3.5-turbo",
+                    messages: [
+                        {
+                            role: "user",
+                            content:
+                                "give me a quote that will help a person get positively motivated to go to the gym. keep it one sentence long.",
+                        },
+                    ],
+                    max_tokens: 5,
+                });
+                const data = await res.choices[0].message;
+                if (data.content !== null) {
+                    setQuote(data.content);
+                }
+                else {
+                    setQuote("I'm sorry, I couldn't find a quote for you");
+                }
+                console.log("the data ", data);
+            } catch (error) {
+                console.log("error ", error);
+            }
         }
-        generateQuote();
-    }, []);
+      
   return (
     <div className="quote-container">
-      <p className="quote">hello {quote}</p>
+      <p className="quote" >hello {quote}</p>
+      <button className="quote-button" onClick={() => generateQuote()}>Get a quote</button>
     </div>
   );
 }
--- app/Motivation/page.tsx
+++ app/Motivation/page.tsx
@@ -1,6 +1,7 @@
 import './motivation.css';
-import { QuoteGenerator } from './QuoteGenerator';
-
+import YoutubeShorts from './YoutubeShorts';
+// import { QuoteGenerator } from './QuoteGenerator';
+const youtubeApiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 export default function Motivation() {
   return (
     <main className='motivation'>
@@ -9,8 +10,11 @@ export default function Motivation() {
           <p className='motivation-title'>Motivation</p>
           <p className='motivation-subtitle'>Find your motivation to keep going</p>
         </div>
+        <div className='motivation-content-container-youtube-shorts'>
+          <YoutubeShorts apiKey={youtubeApiKey}/>
+        </div>
         <div className='motivation-content-container'>
-          <QuoteGenerator />
+          {/* <QuoteGenerator /> */}
         </div>
       </div>
       
