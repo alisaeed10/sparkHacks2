69b2206ab2b72d73820625099fe374d1e776642c
MustafaRush731 <rushmus226@gmail.com>
2024-02-10 03:46:07 -0600

    MustafaChange2

---
+++ components/NavBar.tsx
@@ -0,0 +1,34 @@
+// import Link from 'next/link';
+import {Link} from 'react-scroll';
+
+
+export default function NavBar() {
+
+    return (
+    <nav className ="fixed w-full h-24 shadow-xl bg-white">
+        <div className="flex justify-between items-center h-full w-full px-4 2x1:px-16">
+            {/* <div></div> */}
+            <div>
+                <ul className = "hidden sm:flex">
+                    <li className = "ml-10 uppercase hover:border-b text-xl"><Link to="Home" duration={500} offset={-50}>Home</Link></li>
+                    <li className = "ml-10 uppercase hover:border-b text-xl"><Link to="Motivation" duration={500} offset={-50}>Motivation</Link></li>
+                    <li className = "ml-10 uppercase hover:border-b text-xl"><Link to="AtHomeWorkOuts" duration={500} offset={-50}>At Home WorkOuts</Link></li>
+                    <li className = "ml-10 uppercase hover:border-b text-xl"><Link to="Technique" duration={500} offset={-50}>Technique</Link></li>
+                    {/* <Link href="/Home">
+                        <li className = "ml-10 uppercase hover:border-b text-xl">Home</li>
+                    </Link>
+                    <Link href = "/Motivation">
+                        <li className = "ml-10 uppercase hover:border-b text-xl">Motivation</li>
+                    </Link>
+                    <Link href = "/AtHomeWorkOut">
+                        <li className = "ml-10 uppercase hover:border-b text-xl">Home WorkOuts</li>
+                    </Link>
+                    <Link href = "/Technique">
+                        <li className = "ml-10 uppercase hover:border-b text-xl">Technique</li>
+                    </Link> */}
+                </ul>
+            </div>
+        </div>
+    </nav>
+    )
+};
